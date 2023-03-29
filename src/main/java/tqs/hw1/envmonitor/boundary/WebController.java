6367f83d6b97ec400cfea5eb912d6d6089b8d2e1
João Fonseca <joao.fonseca@ua.pt>
2023-03-29 21:15:56 +0100

    add simple frontend page

--- src/main/java/tqs/hw1/envmonitor/boundary/WebController.java
+++ src/main/java/tqs/hw1/envmonitor/boundary/WebController.java
@@ -1,7 +1,34 @@
 package tqs.hw1.envmonitor.boundary;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tqs.hw1.envmonitor.service.EnvService;
 
 @Controller
 public class WebController {
+
+    private final EnvService envService;
+
+    public WebController(EnvService envService) {
+        this.envService = envService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "layout";
+    }
+
+    @GetMapping("/search")
+    public String current(@RequestParam(value = "q", defaultValue = "") String location, @RequestParam(value = "type", defaultValue = "") String type, Model model) {
+        if (type.equals("current")) {
+            model.addAttribute("env", envService.getCurrentEnv(location));
+        }
+        if (type.equals("forecast")) {
+            model.addAttribute("env", envService.getForecastEnv(location));
+        }
+        model.addAttribute("query", location);
+        return "layout";
+    }
 }
