96a65f77b859a877be46cc355ac4882ca13dda68
João Fonseca <joao.fonseca@ua.pt>
2023-04-02 15:14:37 +0100

    add web controller it

---
+++ src/test/java/tqs/hw1/envmonitor/boundary/WebControllerIT.java
@@ -0,0 +1,63 @@
+package tqs.hw1.envmonitor.boundary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.hamcrest.Matchers.notNullValue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class WebControllerIT {
+
+    @LocalServerPort
+    int localPort;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void whenIndex_thenEmptyIndex() throws Exception {
+        mvc.perform(get("/").contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    void givenNullQuery_whenSearch_thenEmptyIndex() throws Exception {
+        String query = ""; // Maps to null in the Controller
+
+        mvc.perform(get("/search").param("q", query).contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    void givenEmptyQuery_whenSearch_thenEmptyIndex() throws Exception {
+        String query = " ";
+
+        mvc.perform(get("/search").param("q", query).contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    void givenAveiro_whenSearch_thenResultsIndex() throws Exception {
+        String query = "Aveiro";
+
+        mvc.perform(get("/search").param("q", query).contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("query", "Aveiro"))
+                .andExpect(model().attribute("location_country", "Aveiro, PT"))
+                .andExpect(model().attribute("env_current", notNullValue()))
+                .andExpect(model().attribute("env_forecast", notNullValue()));
+    }
+}
