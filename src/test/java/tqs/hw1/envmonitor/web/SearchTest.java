a7f1d018a5ced0efd4a2dfda62b34fff347a27f1
João Fonseca <joao.fonseca@ua.pt>
2023-04-02 17:35:04 +0100

    add frontend test

---
+++ src/test/java/tqs/hw1/envmonitor/web/SearchTest.java
@@ -0,0 +1,40 @@
+package tqs.hw1.envmonitor.web;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import tqs.hw1.envmonitor.web.pages.IndexPage;
+import tqs.hw1.envmonitor.web.pages.SearchPage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SeleniumJupiter.class)
+public class SearchTest {
+
+    @Test
+    void searchInvalidLocation(ChromeDriver driver) {
+        IndexPage indexPage = new IndexPage(driver);
+        indexPage.setQuery("InvalidLocation");
+        indexPage.search();
+
+        SearchPage searchPage = new SearchPage(driver);
+        assertThat(searchPage.getQuery()).isEqualTo("InvalidLocation");
+        assertThat(searchPage.hasCurrentEmpty()).isTrue();
+        assertThat(searchPage.hasForecastEmpty()).isTrue();
+    }
+
+    @Test
+    void searchValidLocation(ChromeDriver driver) {
+        IndexPage indexPage = new IndexPage(driver);
+        indexPage.setQuery("Aveiro");
+        indexPage.search();
+
+        SearchPage searchPage = new SearchPage(driver);
+        assertThat(searchPage.getQuery()).isEqualTo("Aveiro");
+        assertThat(searchPage.getResults()).isEqualTo("Showing results for Aveiro, PT");
+        assertThat(searchPage.hasCurrentTable()).isTrue();
+        assertThat(searchPage.hasForecastChart()).isTrue();
+        assertThat(searchPage.hasForecastTable()).isTrue();
+    }
+}
