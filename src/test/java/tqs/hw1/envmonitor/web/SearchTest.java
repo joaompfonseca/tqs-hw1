61a4eb3103ce927e7e5b230f59cfda56c122ed2c
João Fonseca <joao.fonseca@ua.pt>
2023-04-03 15:56:23 +0100

    fix selenium tests

--- src/test/java/tqs/hw1/envmonitor/web/SearchTest.java
+++ src/test/java/tqs/hw1/envmonitor/web/SearchTest.java
@@ -1,21 +1,31 @@
 package tqs.hw1.envmonitor.web;
 
-import io.github.bonigarcia.seljup.SeleniumJupiter;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import tqs.hw1.envmonitor.web.pages.IndexPage;
 import tqs.hw1.envmonitor.web.pages.SearchPage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SeleniumJupiter.class)
 public class SearchTest {
 
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Test
-    @Disabled
-    void searchInvalidLocation(ChromeDriver driver) {
+    void searchInvalidLocation() {
         IndexPage indexPage = new IndexPage(driver);
         indexPage.setQuery("InvalidLocation");
         indexPage.search();
@@ -27,8 +37,7 @@ public class SearchTest {
     }
 
     @Test
-    @Disabled
-    void searchValidLocation(ChromeDriver driver) {
+    void searchValidLocation() {
         IndexPage indexPage = new IndexPage(driver);
         indexPage.setQuery("Aveiro");
         indexPage.search();
